022421efe6773ccef960a328b94670e2e7f724bc
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-11-06 12:27:02 +0700

    Refactor editor to improve event handling and screen management
    - Added `evaluate_event` function to handle key events, encapsulating logic for user input evaluation.
    - Introduced `initialize` and `clear_screen` methods for setting up and clearing the screen, enhancing readability.
    - Moved screen refresh logic to `refresh_screen`, which clears the screen on exit and displays a farewell message.
    - Simplified main loop to delegate event processing and screen refresh tasks to new helper functions.

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,9 @@
+use std::io::stdout;
+
 use crossterm::event::KeyModifiers;
-use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 
 pub struct Editor {
     should_quit: bool,
@@ -12,28 +15,46 @@ impl Editor {
     }
 
     fn repl(&mut self) -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
         loop {
-            if let Key(KeyEvent {
-                code,
-                modifiers,
-                kind,
-                state,
-            }) = read()?
-            {
-                println!(
-                    "Code: {code:?} Modifiers: {modifiers:?} kind: {kind:?}, state: {state:?}"
-                );
-                match code {
-                    Char('q') if modifiers == KeyModifiers::CONTROL => self.should_quit = true,
-                    _ => (),
-                }
-            }
-            if self.should_quit {
+            let event = read()?;
+            self.evaluate_event(&event);
+            self.refresh_screen()?;
+
+            if self.should_quit == true {
                 break;
             }
         }
-        disable_raw_mode()?;
+        Ok(())
+    }
+
+    fn initialize() -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+
+    fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code, modifiers, ..
+        }) = event
+        {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => self.should_quit = true,
+                _ => (),
+            }
+        }
+    }
+
+    fn refresh_screen(&mut self) -> Result<(), std::io::Error> {
+        if self.should_quit == true {
+            Self::clear_screen()?;
+            print!("Goodbye.\r\n");
+        }
+
         Ok(())
     }
 
